f08968e03432dcd46f6386bc45add2e148e6c78c
SoumyaD27 <soumyad233@gmail.com>
2022-12-18 09:53:48 +0530

    [Permutations of string]

---
+++ PermutationsOfString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    private static void generatePermute(String str, int l, int r)
+    {
+        if (l == r-1)
+            Console.WriteLine(str);
+        else
+            for (int i = l; i <= r; i++)
+            {
+                str = swap(str, l, i);
+                generatePermute(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+    }
+
+    public static String swap(String a, int i, int j)
+    {
+        char ch;
+        char[] b = a.ToCharArray();
+        ch = b[i];
+        b[i] = b[j];
+        b[j] = ch;
+        string s = new string(b);
+        return s;
+    }
+    public static void Main(string[] args)
+    {
+        String str = "ABC";
+        int n = str.Length;
+        Console.WriteLine("All the permutation of the string are: ");
+        generatePermute(str, 0, n);
+    }
+}
