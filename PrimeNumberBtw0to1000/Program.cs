7ca91857359d6aafeb0d7f949b77d26ee92869f3
SoumyaD27 <soumyad233@gmail.com>
2022-12-18 12:53:51 +0530

    [Prime Number between Range 0-1000]

---
+++ PrimeNumberBtw0to1000/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        int num, i, j, stno, enno;
+        Console.WriteLine("Input starting number of range: ");
+        stno= Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Input ending number of range: ");
+        enno = Convert.ToInt32(Console.ReadLine());
+        for(num = stno; num<=enno;num++)
+        {
+            j = 0;
+            for(i=2;i<=num/2;i++)
+            {
+                if(num%i==0)
+                {
+                    j++;
+                    break;
+                }
+            }
+            if(j ==0 && num!=1)
+            {
+                Console.WriteLine("{0}", num);
+            }
+        }
+    }
+}
